e3d9a9ff9022ce6bbeca488fdc84c715a5b2ff35
zhoudaxia <muzhouz@gmail.com>
2024-08-20 00:44:37 +0800

    完善青空文库爬虫

--- reading/aozora.gr.jp/crawl.py
+++ reading/aozora.gr.jp/crawl.py
@@ -4,12 +4,13 @@ import sys
 import re
 
 url = sys.argv[1]
+chapter_pat = sys.argv[2] if len(sys.argv) > 2 else ''
 page = requests.get(url)
 page.encoding = page.apparent_encoding
 soup = BeautifulSoup(page.text, features="html.parser")
 
 main = soup.select_one('.main_text')
 if main:
-  texts = ['' if re.search('○', _) else _ for _ in main.text.strip().splitlines() if _ != '']
+  texts = ['' if chapter_pat and re.search(chapter_pat, _) else _ for _ in main.text.strip().splitlines() if _ != '']
   with open('data.txt', 'w', encoding='utf8') as f:
     f.writelines('\n'.join(texts))
