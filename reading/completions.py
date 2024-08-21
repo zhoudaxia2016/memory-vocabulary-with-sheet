d8af014434df6b302118a67876a579c31ca32a13
zhoudaxia <muzhouz@gmail.com>
2024-08-22 00:45:18 +0800

    feat: 阅读支持翻译

---
+++ reading/completions.py
@@ -0,0 +1,30 @@
+import requests
+from os import environ
+from dotenv import load_dotenv
+load_dotenv()
+completions_url = environ.get('completions_url')
+completions_key = environ.get('completions_key')
+completions_model = environ.get('completions_model')
+
+prompt = '''
+你是一个熟悉日语的大模型，请将下面日语歌词翻译成中文：
+'''
+
+def completions(text):
+  if not completions_url:
+    print('请配置completions_url')
+    return
+  res = requests.post(completions_url, json={
+    'model': completions_model,
+    'messages': [
+      {
+        'role': 'user',
+        'content': prompt + text,
+      }
+    ]
+  }, headers={'Authorization': completions_key, 'content-type': 'application/json'})
+  res =  res.json()
+  print(res)
+  result = res['choices'][0]['message']['content']
+  print('消耗tokens: ', res['usage']['total_tokens'])
+  return result
--- reading/upload.py
+++ reading/upload.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import requests
 import sys
 from parse import parse
+from completions import completions
 
 load_dotenv()
 hook = environ.get('hook')
@@ -21,31 +22,44 @@ if isinstance(hook, str):
     chapters = []
     # chapter由两个换行分割
     for i, chapter_text in enumerate(chapter_texts):
+      translations = completions(chapter_text)
+      if not translations:
+        print('翻译出错', translations)
+        quit()
+      translations = translations.split('\n')
       paragraph_text = chapter_text.split('\n')
       chapter = []
       # paragraph由一个换行分割
       for j, paragraph_text in enumerate(paragraph_text):
         if paragraph_text == '':
           continue
+        paragraph_translation = translations[j].split('。')
+        k = 0
         result = parse(paragraph_text)
         sentences = []
         sentence = newSentence(i, j)
         for word in result:
           section = ''
+          text = ''
           is_end = False
           for token in word['tokens']:
             if token['text'] == '。':
               is_end = True
               break
-            section = section + token['text']
+            text = text + token['text']
+            section = section + token['text'] + ('(' + token['kana'] + ')' if token['base'] != token['kana'] else '')
             if token['tag'] in collect_tags:
               sentence['words'].append({'base': token['base'], 'kana': token['kana']})
           sentence['sections'].append(section)
-          sentence['text'] = sentence['text'] + section
+          sentence['text'] = sentence['text'] + text
           if is_end:
+            sentence['translation'] = paragraph_translation[k]
+            k = k + 1
             sentences.append(sentence)
             sentence = newSentence(i, j)
         if sentence['text']:
+          sentence['translation'] = paragraph_translation[k]
+          k = k + 1
           sentences.append(sentence)
         chapter.append(sentences)
       chapters.append(chapter)
